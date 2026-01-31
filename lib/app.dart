13bf14c08c84bc5b7787a22455996aed1d8b365f
miel2793 <mahmudsifat2793@gmail.com>
2026-01-31 12:51:30 +0600

    add

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_all/home.dart';
  class Myapp extends StatelessWidget
  {
-   Myapp({super.key});
+   const Myapp({super.key});
    @override
   Widget build(BuildContext context) {
     return MaterialApp(
