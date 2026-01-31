cc1a291ad94daa0fcb79c0fa8abe2b0bd211a7b6
miel2793 <mahmudsifat2793@gmail.com>
2026-01-31 17:35:04 +0600

    add

--- lib/buttongroup.dart
+++ lib/buttongroup.dart
@@ -16,27 +16,64 @@ body: Center(
   child: Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
+      Text("Suggestion Sharing\n\t         Platform",style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 35
+      ),),
 
+      SizedBox(
+        height: 50,
+      ),
+
+Text("This productive tool is designed to help\n\t\t\t\tyou better share your suggestion\n\t\t\t\t\tdepartment-wise conveniently! ",style:
+  TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold
+  ),),
+      SizedBox(
+        height: 100,
+      ),
       SizedBox(
         width: 300,
         height: 60,
         child: ElevatedButton(
+
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.deepPurpleAccent,
+              foregroundColor: Colors.amber,
+
+
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+
+              )
 
 
             ),
-            onPressed: (){}, child: Text("Let's Start",style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: Colors.white
+            onPressed: (){}, child:
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Center(
+                  child: Text(
+                    "Let's Start\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
 
+                  ),
+                ),
+                Icon(Icons.arrow_forward_rounded),
+                SizedBox(width: 10),
 
-        ),)
+              ],
 
+            )
         ),
-      )
+      ),
+
+
+
     ],
+
 
   ),
 ),
