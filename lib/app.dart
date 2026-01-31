984b97bffc64a2daaea581029961a71e6dcd33b4
miel2793 <mahmudsifat2793@gmail.com>
2026-01-31 22:16:42 +0600

    add

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_all/home.dart';
 import 'button_icon.dart';
 import 'buttongroup.dart';
+import 'text _field.dart';
  class Myapp extends StatelessWidget
  {
     Myapp({super.key});
@@ -10,7 +11,7 @@ import 'buttongroup.dart';
     return MaterialApp(
       title: 'practice app',
       debugShowCheckedModeBanner: false,
-      home: Buttonicon(),
+      home: Textfield(),
 
     );
   }
---
+++ lib/text _field.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+class Textfield extends StatelessWidget {
+  const Textfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TextField & Input"),
+      ),
+      
+    );
+  }
+}
