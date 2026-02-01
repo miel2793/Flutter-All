49ff3553ede5e3cf3f9c3c1f8a776ceb93d83b2e
miel2793 <mahmudsifat2793@gmail.com>
2026-02-01 14:54:03 +0600

    add

--- lib/textformfield.dart
+++ lib/textformfield.dart
@@ -6,83 +6,125 @@ class Textfrom extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController phone =TextEditingController();
     TextEditingController pass =TextEditingController();
+    final _formkey =GlobalKey<FormState>();
     return Scaffold(
        appBar: AppBar(
          title: Text("TextFormField"),
          backgroundColor: Color.fromRGBO(28, 214, 214,0.4 ),
        ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-               children: [
-                 SizedBox(
-                     child: Image.asset('assets/images/p1.png')),
-                 SizedBox(
-                   height: 30,
-                 ),
-                 Text('Log In With Phone Number & Password',style: TextStyle(
-                   fontSize: 15,
-                   fontWeight: FontWeight.bold
-                 ),),
-                 Padding(
-                   padding: const EdgeInsets.only(right: 30,left:30,top: 20,bottom: 0),
-                   child: TextFormField(
-                     controller: phone,
-                     maxLength: 11,
-                     keyboardType:TextInputType.phone,
-                     decoration: InputDecoration(
-                       prefixIcon: Icon(Icons.phone),
-                       suffixIcon: Icon(Icons.check),
-                       labelText: "Enter Your Name",
-                       hintText: '+88',
-                       helperText: 'Enter with 01',
-                       border: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(16)
+        child: Form(
+
+          key:_formkey ,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                 children: [
+                   SizedBox(
+                       child: Image.asset('assets/images/p1.png')),
+                   SizedBox(
+                     height: 30,
+                   ),
+                   Text('Log In With Phone Number & Password',style: TextStyle(
+                     fontSize: 15,
+                     fontWeight: FontWeight.bold
+                   ),),
+                   Padding(
+                     padding: const EdgeInsets.only(right: 30,left:30,top: 20,bottom: 0),
+                     child: TextFormField(
+                       validator:(value) {
+                         if(value ==null)
+                           {
+                             return 'Enter Your Phone Number';
+                           }
+                          else if( value.length !=11)
+                            {
+                             return 'Enter Correct Number';
+                            }
+                          else
+                            {
+                              return null;
+                            }
+
+
+                       },
+                       controller: phone,
+                       maxLength: 11,
+                       keyboardType:TextInputType.phone,
+                       decoration: InputDecoration(
+                         prefixIcon: Icon(Icons.phone),
+                         suffixIcon: Icon(Icons.check),
+                         labelText: "Enter Your Name",
+                         hintText: '+88',
+                         helperText: 'Enter with 01',
+                         border: OutlineInputBorder(
+                           borderRadius: BorderRadius.circular(16)
+                         ),
+
+
                        ),
 
-        
                      ),
-        
                    ),
-                 ),
-                 Padding(
-                   padding: const EdgeInsets.only(right: 30,left:30,top: 20,bottom: 0),
-                   child: TextFormField(
-                     controller:  pass,
-                     obscureText: true,
-                    // keyboardType:TextInputType.phone,
-                     decoration: InputDecoration(
-                         prefixIcon: Icon(Icons.password),
-                         suffixIcon: Icon(Icons.lock),
-                         labelText: "Enter A Password",
-                         hintText: 'M#*^342',
-                         helperText: 'Give a Strong  Password',
-                         border: OutlineInputBorder(
-                             borderRadius: BorderRadius.circular(16)
-                         )
+                   Padding(
+                     padding: const EdgeInsets.only(right: 30,left:30,top: 20,bottom: 0),
+                     child: TextFormField(
+                       validator:(value) {
+                         if(value ==null)
+                         {
+                           return 'Enter  password ';
+                         }
+                         else if( value.length <8)
+                         {
+                           return  'Enter Minimum 8  character';
+                         }
+                         else
+                         {
+                           return null;
+                         }
+
+                       },
+                       controller:  pass,
+                       obscureText: true,
+                      // keyboardType:TextInputType.phone,
+                       decoration: InputDecoration(
+                           prefixIcon: Icon(Icons.password),
+                           suffixIcon: Icon(Icons.lock),
+                           labelText: "Enter A Password",
+                           hintText: 'M#*^342',
+                           helperText: 'Give a Strong  Password',
+                           border: OutlineInputBorder(
+                               borderRadius: BorderRadius.circular(16)
+                           )
+
+                       ),
 
                      ),
-
                    ),
-                 ),
-                  SizedBox
-                    (
-                    height: 30,
-                  ),
-                 
-                  SizedBox(
-height: 50,
-                      width: 200,
-                      child: ElevatedButton(onPressed: (){
+                    SizedBox
+                      (
+                      height: 30,
+                    ),
 
-                        print(phone.text);
-                        phone.clear();
-                        print(pass.text);
-                        pass.clear();
-                      }, child:Text('Log in') ))
-               ],
+                    SizedBox(
+          height: 50,
+                        width: 200,
+                        child: ElevatedButton(onPressed: (){
+                          if(_formkey.currentState!.validate())
+                            {
+                              print(phone.text);
+                              print(pass.text);
+
+                            }
+
+
+                          phone.clear();
+
+                          pass.clear();
+                        }, child:Text('Log in') ))
+                 ],
+              ),
             ),
           ),
         ),
