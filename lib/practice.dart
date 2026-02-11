41270cae300ebf56a65928f403a91a8e8ea6c441
miel2793 <mahmudsifat2793@gmail.com>
2026-02-11 23:53:59 +0600

    add

--- lib/practice.dart
+++ lib/practice.dart
@@ -6,7 +6,99 @@ class All extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Color Shade",style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+         // backgroundColor: Colors.cyanAccent[200]
+        ),),
+        backgroundColor: Colors.cyan[200],
+        centerTitle: true,
 
+      ),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8,left: 10),
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    color: Colors.black,
+                  ),
+                ),
+
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
